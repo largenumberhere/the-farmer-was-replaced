b6015526da3d60ac803ecf62395cc129085e8c77
largenumberhere <largeNumbergoeshere@gmail.com>
2025-10-16 22:41:58 +1100

    2025-10-16 22:41

--- cactus2.py
+++ cactus2.py
@@ -1,5 +1,7 @@
 import mover
 
+
+set_world_size(16)
 max_pos = get_world_size()
 
 op_dir = {North:South, South:North, East:West, West:East}
@@ -13,13 +15,66 @@ def ensure_planted():
 		plant(Entities.Cactus)
 
 def measure2(direction=None):
+	max = 999999999
+	min = -1
 	if direction==North and get_pos_y() == get_world_size() -1:
-		return 999999999
+		return max
+	elif direction==East and get_pos_x() == get_world_size()-1:
+		return max
 	
-	if direction == West and get_pos_x() == 0:
-		return -1
+	elif direction == West and get_pos_x() == 0:
+		return min
 	
-	return measure(direction)
+	elif direction == South and get_pos_y() == 0:
+		return min
+	else:
+		return measure(direction)
+
+def sort_h():
+	actioned = False
+	l = measure2(West)
+	r = measure2(East)
+	h = measure2()
+	
+	if l!=None and h!=None and h < l:
+		actioned = True
+		swap(West)
+		(h, l) = (l,h)
+	
+	if r!=None and h!=None and r < h:
+		actioned = True
+		swap(East)
+		(r, h) = (h, r)
+	
+	if h!=None and l!=None and h < l:
+		actioned = True
+		swap(West)
+		(h, l) = (l, h)
+	
+	return actioned
+
+def sort_v():
+	actioned = False
+	u = measure2(North)
+	h = measure2()
+	d = measure2(South)
+	
+	if h!=None and d!=None and h < d:
+		actioned = True
+		swap(South)
+		(h, d) = (d, h)
+		
+	if u!=None and h!=None and u < h:
+		actioned = True
+		swap(North)
+		(u, h) = (h, u)
+	
+	if h!=None and d!=None and h < d:
+		actioned = True
+		swap(South)
+		(h, d) = (d, h)
+	
+	return actioned
 
 def delay(ticks):
 	for i in range(ticks):
@@ -43,67 +98,74 @@ def move_x_end():
 
 mover.move_to(0,0)
 while True:
-	#set_execution_speed(10)
 	# sort columns
 	for col in range(max_pos):
-		while get_pos_y() < max_pos:
+		# plant the column
+        while get_pos_y() < max_pos:
 			ensure_tilled()
 			ensure_planted()
 			if get_pos_y() != max_pos-1:
+                sort_v()
 				move(North)
 			else:
 				break
 		
-		delay(10)
-
+        # sort the column
 		bad = True
 		while bad:
 			bad = False
 			while get_pos_y() >0:
-				north = measure2(North)
-				here = measure2()
-				south = measure2(South)
-				
-				if not north >= south:
-					swap(South)
-					swap(North)
-					bad = True
-					south = measure2()
-					north = measure2()
-				
-				if not here >= south:
-					swap(South)
-					south = measure2()
-					here = measure2()
-					bad = True
+				actioned = sort_v()
+                if actioned:
+                    bad = True
 					
 				move(South)
-			 
-			move_y_end()
+			if not bad:
+                break
+            
+            bad = False
+			while get_pos_y() < max_pos-1:
+                actioned = sort_v()
+                if actioned:
+                    bad = True
+                
+                move(North)
+                
+                
 			
 		move_y_start()
 		move(East)
 		
 	# sort rows
 	for row in range(max_pos):
-		here = measure2()       
-		move_x_start()
+		while get_pos_x() >0:
+            move(West)
+            sort_h()
 		bad = True
 		while bad:
 			bad = False
 			
 			for col in range(max_pos):
-				if measure2() < measure2(West):
-					swap(West)
-					bad = True
-				
-				move(East)
-				
-				
-				
+                actioned = sort_h()
+                if actioned:
+                    bad = True
+                
+                if col != max_pos-1:
+                    move(East)
+                
+            if not bad:
+                break
+            
+            for col in range(max_pos):
+                actioned = sort_h()
+                if actioned:
+                    bad = True
+                
+                if col != max_pos-1:
+                    move(West)
+            
 		move(North)
-	   
-	harvest()
+        
 	move_x_start()
 	move_y_start()
-	
+	harvest()
